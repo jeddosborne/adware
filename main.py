dd75c86c154a57f44938875b55202709cac458bf
Jedd Osborne <jeddosborne@icloud.com>
2024-08-31 14:50:29 +0100

    there u go jared

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import ctypes
 import playsound
 
-WALLPAPER_PATH = "C:\\Users\\jeddo\\Documents\\CODE\\adware-1\\assets\\trumpshooting.webp"
+WALLPAPER_PATH = "assets\\trumpshooting.webp"
 # 0: Center, 1: Stretch, 2: Tile, 6: Fit
 WALLPAPER_STYLE = 1
 # Set the wallpaper
@@ -10,5 +10,5 @@ ctypes.windll.user32.SystemParametersInfoW(20, 0, WALLPAPER_PATH, WALLPAPER_STYL
 print("part 1 successful")
 
 #Play sound
-playsound.playsound("C:\\Users\\jeddo\\Documents\\CODE\\adware-1\\assets\\buildawall.mp3")
+playsound.playsound("assets\\buildawall.mp3")
 print("part 2 successful")
